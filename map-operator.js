d5453b10b9565958aeb5357721ad55e24b34e95a
Chris Bitsakis <c.bitsakis@gmail.com>
2016-04-21 03:07:28 -0400

    add support for deep method calls
    still not passing all tests

--- map-operator.js
+++ map-operator.js
@@ -3,91 +3,135 @@
 (() => {
     'strict mode';
 
-    Object.defineProperty(Array.prototype, '_mapOperator', {
-        get() {
-            // var _typeOf = function() {
-            //     Object.assign(propertiesInArray, {
-            //         // PropertyName: PropertyType
-            //         [propertyName]: (() => {
-            //             if(property === null) {
-            //                 return 'null';
-            //             } else if (property instanceof Function) {
-            //                 return 'function';
-            //             } else {
-            //                 return typeof property;
-            //             }
-            //         })()
-            //     });
-            // };
-            var propertiesInArray = {};
+    var mapOperatorName = '_mapOperator';
+
+    var exceptions = {
+        expectingFunction: 'A function was expected in the map operator'
+    }
+
+    Object.defineProperty(Array.prototype, mapOperatorName, {
+        // obj[]
+        get() { // put this in a function above Object.defineProperty
+            var self = this; // give this a better name if possible
+            // obj[](...);
             var mappedArray = function mappedArray() {
+                if(typeof arguments[0] === 'undefined') throw exceptions.expectingFunction;
+
                 var args = Array.from(arguments);
-                var isFunction = args[0] instanceof Function;
+                var fn = args[0];
+                var hasContext = self._mapOperatorContext;
+                var baseArray = hasContext ? self._mapOperatorContext.base : self;
+                var mapOperatorPath = hasContext ? self._mapOperatorContext.path : [];
 
-                if(isFunction) {
-                    var result = this.map(args[0], this);
+                var propertiesPath = mapOperatorPath.filter(p => p !== '_mapOperator');
+                var noDeepAccess = propertiesPath.length === 0;
+                var propertiesPathIndex = 0;
+                var isLastPath = () => propertiesPathIndex === propertiesPath.length - 1;
 
-                    // Replace the array with the mapped result
-                    // if passing a non-function
-                    this.splice(0);
-                    this.concat(result);
+                var makeSetTargetProperty = index => {
+                    var property = propertiesPath[index];
+                    var setTargetProperty = obj => { // a[]
+                        obj[property] = fn.call(self, obj[property]);
 
-                    return result
-                    // return this.map(args[0], this);
+                        return obj;
+                    };
+
+                    return setTargetProperty;
+                };
+
+                var makeAccessProperty = index => {
+                    var property = propertiesPath[index];
+                    var accessProperty = obj => {
+                        obj[property] = obj[property].map(makeSetTargetProperty(index + 1)); // obj[property + 1] ? makeAccessProperty(0) : makeSetTargetProperty(0)
+
+                        return obj;
+                    };
+
+                    return accessProperty;
+                };
+
+                if(noDeepAccess) {
+                    var result = baseArray.map(fn);
                 } else {
-                    var result = this.map(() => JSON.parse(JSON.stringify(args[0])), this);
-
-                    // Replace the array with the mapped result
-                    // if passing a non-function
-                    this.splice(0);
-                    this.concat(result);
-
-                    return result
+                    var init = isLastPath()  ? makeSetTargetProperty(0) : makeAccessProperty(0);
+                    var result = baseArray.map(init);
                 }
+                //! throw if mapOperator accessed twice in a row? [][]
+                //! any reason to do that?
+
+                // return hasContext ? self._mapOperatorContext.base : self;
+                // self.map(obj => args[0].call(self, obj));
+                return result;
             };
 
-            // Add the keys of every property in each
-            // of the array's objects to the propertiesInArray object.
-            this.forEach(objInArray => {
+            var propertiesInArray = {}; // rename to propertyMetadata
+
+            var getPropertiesInArray = (objInArray, i, array) => {
                 if(!(objInArray instanceof Object)) return;
 
+                var isArray = objInArray instanceof Array;
+                var hasContext = '_mapOperatorContext' in self;
+                var objPath = hasContext ? self._mapOperatorContext.path : [];
+
+                // If the array has a _mapOperatorContext and it is an array
+                // then get the properties of each of the arrays n levels in.
+                if(isArray && hasContext && hasContext) {
+                    objPath.forEach(property => {
+                        if (property === mapOperatorName) return;
+
+                        objInArray = objInArray[0];
+                    });
+                }
+
+                var arrayIndices = Object.keys(array);
                 var objectPrototype = Object.getPrototypeOf(objInArray);
+                var propertiesInPrototype = Object.getOwnPropertyNames(objectPrototype);
                 // Get the keys of the object, including the keys of its direct parent prototype
-                var propertiesInObject = Object.keys(objInArray).concat(Object.getOwnPropertyNames(objectPrototype));
+                var propertiesInObject = Object.keys(objInArray).concat(propertiesInPrototype).concat(arrayIndices);
 
                 // For each property of this object,
                 // record its type so its behavior can be set later
                 propertiesInObject.forEach(propertyName => {
                     var property = objInArray[propertyName];
+                    var propertyType = (() => {
+                        if(property === null) {
+                            return 'null';
+                        } else if (property instanceof Function) {
+                            return 'function';
+                        } else {
+                            return typeof property;
+                        }
+                    })();
 
-                    Object.assign(propertiesInArray, {
-                        // PropertyName: PropertyType
-                        [propertyName]: (() => {
-                            if(property === null) {
-                                return 'null';
-                            } else if (property instanceof Function) {
-                                return 'function';
-                            } else {
-                                return typeof property;
-                            }
-                        })()
-                    });
+                    // Don't redefine the method if it has already
+                    // been defined in a prior object in the array.
+                    // If one of existingType and propertyType is a function
+                    // and the other isn't, throw error.
+                    // if (propertyName in propertiesInArray) {
+                    //     var existingType = propertiesInArray[propertyName];
+                    //     ...
+                    // }
+
+                    // Avoid breaking _mapOperatorContext. Might not be needed?
+                    if(propertyName === '_mapOperatorContext') return;
+
+                    propertiesInArray[propertyName] = {
+                        type: propertyType,
+                    };
                 });
-            });
+            };
 
-            // Create getters/setters or functions mapped to each
-            // of the original array's object's properties.
-            Object.keys(propertiesInArray).forEach(propertyName => {
-                var propertyType = propertiesInArray[propertyName];
-                var self = this;
+            // Create a property in the map operator
+            var makeProperty = propertyName => {
+                var property = propertiesInArray[propertyName];
 
-                if(propertyName in mappedArray) return; //! this skips non-enumerable methods like constructor and __proto__. yay or nay?
+                if(propertyName in mappedArray) return;
 
-                if(propertyType === 'function') {
+                if(property.type === 'function') {
                     Object.defineProperty(mappedArray, propertyName, {
+                        // obj[].method()
                         value: function() { // (...arguments) =>
                             return self.map(objInArray => {
-                                console.log(objInArray)
                                 return objInArray[propertyName](...Array.from(arguments));
                             });
                         },
@@ -95,28 +139,99 @@
                     });
                 } else {
                     Object.defineProperty(mappedArray, propertyName, {
-                        get: () => {
-                            return this.map(objInArray => {
+                        // obj[].property;
+                        get: function() {
+                            var unwrapEachInside = target => {
+                                return target.reduce((arr, property) => {
+                                    if (property === mapOperatorName) return;
+
+                                    arr.push(...property);
+
+                                    return arr;
+                                }, []);
+                            };
+                            var hasDepth = self._mapOperatorContext && self._mapOperatorContext.path.length;
+                            var target = hasDepth ? unwrapEachInside(self) : self;
+                            var mappedArray = target.map(objInArray => {
+                                // Nothing to access, just return the property's value
                                 if(!(objInArray instanceof Object)) return objInArray;
 
                                 var propertyValue = objInArray[propertyName];
 
                                 return propertyValue;
                             });
+
+                            // Add property to the context chain
+                            if('_mapOperatorContext' in this) {
+                                // Inherit existing _mapOperatorContext
+                                Object.defineProperty(mappedArray, '_mapOperatorContext', {
+                                    value: this._mapOperatorContext,
+                                    configurable: true
+                                });
+                                mappedArray._mapOperatorContext.path.push(propertyName);
+                            } else {
+                                // Create record of the original object's context
+                                Object.defineProperty(mappedArray, '_mapOperatorContext', {
+                                    value: {
+                                        base: self,
+                                        path: []
+                                    },
+                                    configurable: true
+                                });
+                            }
+
+                            return mappedArray;
                         },
-                        set: (value) => {
-                            this.forEach(objInArray => {
+                        // obj[].property = 42;
+                        set: function(value) {
+
+                            // this can use similar logic as mappedArray()
+
+                            self.forEach(objInArray => {
                                 if(!(objInArray instanceof Object)) return;
 
                                 objInArray[propertyName] = value;
                             });
 
-                            return this;
+                            if (self._mapOperatorContext) {
+                                return self._mapOperatorContext.base;
+                            } else {
+                                return self;
+                            }
                         },
                         configurable: true
                     });
                 }
-            });
+            };
+
+            // Has the map operator been used on this object?
+            if('_mapOperatorContext' in self) {
+                // Pass the context to the map operator
+                Object.defineProperty(mappedArray, '_mapOperatorContext', {
+                    value: self._mapOperatorContext,
+                    configurable: true
+                });
+                mappedArray._mapOperatorContext.path.push(mapOperatorName);
+            } else {
+                // Create record of the original object's context
+                Object.defineProperty(mappedArray, '_mapOperatorContext', {
+                    value: {
+                        base: self,
+                        path: []
+                    },
+                    configurable: true
+                });
+            }
+
+            // Add the keys of every property in each
+            // of the array's objects to the propertiesInArray object.
+            self.forEach(getPropertiesInArray);
+
+
+            // Create getters/setters or functions mapped to each
+            // of the original array's object's properties.
+            Object.keys(propertiesInArray).forEach(makeProperty);
+
 
             return mappedArray;
         }
--- test.js
+++ test.js
@@ -5,19 +5,19 @@ const mapOperatorKey = '_mapOperator';
 
 // assume obj[mapOperatorKey] === obj[]
 
-var evalMapOperator = code => {
-    var mapOperator = /\[\]/g;
-    // Not a perfect way to detect usage of [] operator but good enough for a demo
-    var mapOperatorHook = /[^\s=:](\[\])+/g;
+// var evalMapOperator = code => {
+//     var mapOperator = /\[\]/g;
+//     // Not a perfect way to detect usage of [] operator but good enough for a demo
+//     var mapOperatorHook = /[^\s=:](\[\])+/g;
 
-    code.replace(mapOperatorHook, function(match) {
-        var replaceWith = match.replace(mapOperator, "[\'" + mapOperatorKey + "\']");
+//     code.replace(mapOperatorHook, function(match) {
+//         var replaceWith = match.replace(mapOperator, "[\'" + mapOperatorKey + "\']");
 
-        code = code.replace(match, replaceWith)
-    });
+//         code = code.replace(match, replaceWith)
+//     });
 
-    return code;
-}
+//     return code;
+// }
 
 test('map operator loaded', t => {
     let arr = [][mapOperatorKey];
@@ -67,18 +67,28 @@ test('custom function call on each object in array', t => {
     t.deepEqual(list, [2, 4, 6, 8, 10]);
 });
 
-test('custom method call without function', t => {
-    let list = [1, 2, 3, 4, 5];
+test('keep record of index in map operator', t => {
+    let list = [10, 20, 30, 40, 50];
 
-    list = list[mapOperatorKey](0);
+    list = list[mapOperatorKey]((n, i) => (i + 1) + n);
 
-    t.deepEqual(list, [0, 0, 0, 0, 0]);
+    t.deepEqual(list, [11, 22, 33, 44, 55]);
+});
+
+// keep record of index in map operator + deep
+
+test('set all properties in array of objects', t => {
+    let list = [{ num: 1 }, { num: 2 }, { num: 3 }, { num: 4 }, { num: 5 }];
+
+    list[mapOperatorKey].num = 10;
+
+    t.deepEqual(list, [{ num: 10 }, { num: 10 }, { num: 10 }, { num: 10 }, { num: 10 }]);
 });
 
 test('call inherited prototype method on properties in array', t => {
     let list = [[0], [0], [0]];
 
-    list = list[mapOperatorKey]([10]);
+    list = list[mapOperatorKey](n => [10]);
     list[mapOperatorKey].push(20);
 
     t.deepEqual(list, [[10, 20], [10, 20], [10, 20]]);
@@ -86,19 +96,58 @@ test('call inherited prototype method on properties in array', t => {
 
 
 test('deep array manipulation', t => {
-    let albums = [{
-        editions: [15, 20]
-    }, {
-        editions: [30, 40]
-    }];
+    var list = [
+        { a: [{ b: 10 }, { b: 15 }] },
+        { a: [{ b: 20 }, { b: 25 }] }
+    ];
 
-    albums = albums[mapOperatorKey].editions[mapOperatorKey][mapOperatorKey](edition => edition * 10);
+    list = list[mapOperatorKey].a[mapOperatorKey].b[mapOperatorKey](n => n * 2);
 
-    // console.log(albums);
+    t.deepEqual(list, [
+        { a: [{ b: 20 }, { b: 30 }] },
+        { a: [{ b: 40 }, { b: 50 }] }
+    ]);
+})
 
-    t.deepEqual(albums, [{
-        editions: [150, 200]
-    }, {
-        editions: [300, 400]
-    }]);
-});
+// deep set all properties in objects
+
+
+// test('deep selection of index in array', t => {
+//     var list = [
+//         { a: [{ b: 10 }, { b: 15 }] },
+//         { a: [{ b: 20 }, { b: 25 }] }
+//     ];
+
+//     list = list[mapOperatorKey].a[0].b;
+
+//     t.deepEqual(list, [10, 20]);
+// });
+
+// deep property set, get, method call, fn.
+
+
+// test('throw on mismatched property types', t => {
+
+// });
+
+
+
+// list[mapOperatorKey].a[mapOperatorKey].b[mapOperatorKey] = 30
+
+// test('deep array manipulation', t => {
+//     let albums = [{
+//         editions: [15, 20]
+//     }, {
+//         editions: [30, 40]
+//     }];
+
+//     albums = albums[mapOperatorKey].editions[mapOperatorKey][mapOperatorKey](edition => edition * 10);
+
+//     // console.log(albums);
+
+//     t.deepEqual(albums, [{
+//         editions: [150, 200]
+//     }, {
+//         editions: [300, 400]
+//     }]);
+// });
